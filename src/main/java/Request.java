08dd157ce190f1dfb612cf79a634141a2c007ccb
TimZel <107506944+TimZel@users.noreply.github.com>
2022-11-25 05:11:08 +0300

    Update Request.java

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -8,9 +8,6 @@ public class Request {
     String path;
     String http;
     Map<String, String> mapHeadersValues;
-//    String getContentType(Map<String, String> mapHeadersValues) {
-//        return mapHeadersValues.get("Content-Type:");
-//    }
     String getContentType(String exe) throws FileNotFoundException {
         Pattern patternExeContentType = Pattern.compile(exe + "(.+)");
         String fileName = "/home/zelyanin/IdeaProjects/servReq2.0/src/main/java/testerFiletable";
@@ -94,62 +91,3 @@ public class Request {
 
 
 
-
-
-//(^[^\n\p{Lu}].+) body
-//(^\p{Lu}{3,10}[^\s\d\a-z]*) method
-//(^\p{Lu}{1}[^\s\:]*)headers  (^\p{Lu}[^\s]*\:)
-// (\:.+) :headersValues
-//(^\p{Lu}[^\s]*\:.+) headers+values
-
-//path  (\/[^\s]*)
-//exe (\.[^\s]*)
-
-//Pattern patternPathExeHttp = Pattern.compile("(^\\p{Lu}{3,7}.*)");
-//Matcher matcherPathExeHttp = patternPathExeHttp.matcher(words[i]);
-
-//        "POST /cgi-bin/process.cgi HTTPS/1.1" + CRLF +
-//        "User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)" + CRLF +
-//        "Host: www.tutorialspoint.com"+ CRLF +
-//        "Content-Type: application/x-www-form-urlencoded" + CRLF +
-//        "Content-Length: length" + CRLF +
-//        "Accept-Language: en-us"+ CRLF +
-//        "Accept-Encoding: gzip, deflate"+ CRLF +
-//        "Connection: Keep-Alive"+ CRLF +
-//        CRLF +
-//        "licenseID=string&content=string&/paramsXML=string" + CRLF;
-
-//        Pattern patternExe = Pattern.compile("(\\.[^s]*)");
-//        Matcher matcherExe = patternExe.matcher(path);
-//        String exe = "";
-//        if(matcherExe.find()) {
-//            exe = matcherExe.group();
-//        }
-
-//        String request;
-//        List<String> list = new ArrayList<>();
-//        try {
-//            while((request = br.readLine()) != null){
-//                Scanner scanner = new Scanner(request);
-//                while (scanner.hasNext()) {
-//                   list.add(String.valueOf(scanner.nextLine().split(CRLF)));
-//                }
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-//        for (int i = 0; i < list.size(); i++) {
-//            Matcher matcherBody = patternBody.matcher(list.get(i));
-//            Matcher matcherHeaderValue = patternHeaderValue.matcher(list.get(i));
-//            pathExeHttp = list.get(0);
-//            if(matcherHeaderValue.find()) {
-//                mapHeadersValues.put(matcherHeaderValue.group(1), matcherHeaderValue.group(2));
-//                if(matcherHeaderValue.group(1).equals("Content-Type:")) {
-//                    contentType = matcherHeaderValue.group(2);
-//                }
-//            }
-//            if(matcherBody.find()) {
-//                body = list.get(i);
-////            }
-//        }
